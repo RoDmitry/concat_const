e716cb02e96c505acd010d5d4d202d96e9be19a2
RoDmitry <gh@rdmtr.com>
2023-10-03 23:41:32 +0400

    More tests

--- src/lib.rs
+++ src/lib.rs
@@ -53,15 +53,22 @@ mod tests {
         const LEN4: usize = crate::len_sum(TO_CONCAT4);
         const GREETING4: [u8; LEN4] = crate::concat(TO_CONCAT4);
 
+        const TO_CONCAT6: &[&[u8]] = &[HELLO_BYTES, COMMA_BYTES, WORLD_BYTES, DOT_BYTES, b" ", b"end"];
+        const LEN6: usize = crate::len_sum(TO_CONCAT6);
+        const GREETING6: [u8; LEN6] = crate::concat(TO_CONCAT6);
+
         assert_eq!(&GREETING2, b"Helloworld");
         assert_eq!(&GREETING3, b"Hello, world");
         assert_eq!(&GREETING4, b"Hello, world.");
+        assert_eq!(&GREETING6, b"Hello, world. end");
 
         const GREETING2_STR: &str = unsafe { core::str::from_utf8_unchecked(&GREETING2) };
         const GREETING3_STR: &str = unsafe { core::str::from_utf8_unchecked(&GREETING3) };
         const GREETING4_STR: &str = unsafe { core::str::from_utf8_unchecked(&GREETING4) };
+        const GREETING6_STR: &str = unsafe { core::str::from_utf8_unchecked(&GREETING6) };
         assert_eq!(GREETING2_STR, "Helloworld");
         assert_eq!(GREETING3_STR, "Hello, world");
         assert_eq!(GREETING4_STR, "Hello, world.");
+        assert_eq!(GREETING6_STR, "Hello, world. end");
     }
 }
